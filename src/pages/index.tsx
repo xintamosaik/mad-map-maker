11d99cc179664601587cd08300bac186eeaf4b84
Ulf Dellbrügge <xintamosaik@gmail.com>
2023-04-09 21:17:35 +0200

    change

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -18,7 +18,7 @@ const Home: NextPage = () => {
       <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#3b393a] to-[#323331]">
         <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
           <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
-            <span className="text-stone-300">Mad</span> Map Maker 
+            <span className="text-stone-600">Mad</span> Map Maker 
           </h1>
           <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8">
            
